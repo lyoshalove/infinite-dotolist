57bfed6f0cd5be0ffcc50bff4f08b2d269426e56
Lyosha Zverev <alyoshkazverev20@gmail.com>
2023-09-26 13:25:20 +0400

    feat: show info if todos not exist

--- src/entity/todo/ui/todolist/todolist.tsx
+++ src/entity/todo/ui/todolist/todolist.tsx
@@ -3,8 +3,13 @@ import { observer } from 'mobx-react-lite';
 import { todos } from 'src/app/store';
 
 import { TodoItem } from '../todo-item';
+import styles from './styles.module.scss';
 
 export const Todolist = observer(() => {
+  if (todos.todoArray.length === 0) {
+    return <h1 className={styles.title}>You don't have todos</h1>;
+  }
+
   return (
     <div>
       {todos.todoArray.map((todo) => {
